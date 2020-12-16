adc7346c1e6b4c27832ade5c8049dedfc1eee48a
J Gregory <john.gregory@admiralfinancialservices.co.uk>
2020-12-16 09:07:31 +0000

    Add 2020 day 8 part 2

--- 2020/pkg/game/game.go
+++ 2020/pkg/game/game.go
@@ -19,6 +19,8 @@ func Answer() (int, int, error) {
 		return 0, 0, err
 	}
 
+	data = bytes.TrimRight(data, "\n")
+
 	instructions := bytes.Split(data, []byte("\n"))
 	result, err := Process(instructions)
 	if err != nil {
@@ -27,12 +29,64 @@ func Answer() (int, int, error) {
 		}
 	}
 
-	return result, -1, nil
+	resultFixed, err := ProcessFix(instructions)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	return result, resultFixed, nil
 }
 
-var ErrSeen = errors.New("Already seen")
+func flipInstruction(instructions Instructions, index int) {
+	if bytes.HasPrefix(instructions[index], []byte("jmp")) {
+		instructions[index] = bytes.Replace(instructions[index], []byte("jmp"), []byte("nop"), 1)
+		return
+	}
+	instructions[index] = bytes.Replace(instructions[index], []byte("nop"), []byte("jmp"), 1)
+}
 
-func Process(instructions [][]byte) (int, error) {
+// Instructions is an executable program
+type Instructions [][]byte
+
+// Custom errors for exit states
+var (
+	ErrSeen = errors.New("Already seen")
+	ErrEOP  = errors.New("End of program")
+)
+
+// ProcessFix performs a rolling fix-and-process until the program
+// executes until a proper End Of Program.
+func ProcessFix(instructions Instructions) (result int, err error) {
+	flipped := 0
+	flipInstruction(instructions, flipped)
+	for {
+		result, err = Process(instructions)
+		if err != nil {
+			if errors.Is(err, ErrEOP) {
+				return result, nil
+			}
+			if !errors.Is(err, ErrSeen) {
+				// Genuine error
+				return 0, err
+			}
+			// We saw an instruction twice, so we
+			// attempt a fix and then re-run.
+		}
+		i := 0
+		for i = flipped + 1; i < len(instructions); i++ {
+			if bytes.HasPrefix(instructions[i], []byte("jmp")) || bytes.HasPrefix(instructions[i], []byte("nop")) {
+				flipInstruction(instructions, i)
+				break
+			}
+		}
+		// Flip the old one back
+		flipInstruction(instructions, flipped)
+		flipped = i
+	}
+}
+
+// Process runs an instruction set
+func Process(instructions Instructions) (int, error) {
 
 	accumulator := 0
 	seen := make(map[int]bool)
@@ -42,7 +96,6 @@ func Process(instructions [][]byte) (int, error) {
 		if err != nil {
 			return 0, errors.WithMessage(err, "failed to parse instructions")
 		}
-		// fmt.Printf(" - %s (%s%d)\n", op, sign, value)
 
 		if seen[i] {
 			return accumulator, ErrSeen
@@ -66,7 +119,7 @@ func Process(instructions [][]byte) (int, error) {
 			i -= value + 1
 		}
 	}
-	return accumulator, nil
+	return accumulator, ErrEOP
 }
 
 var reInstruction = regexp.MustCompile(`(nop|acc|jmp) ([+-])(\d+)`)
--- 2020/pkg/game/game_test.go
+++ 2020/pkg/game/game_test.go
@@ -12,7 +12,7 @@ func TestAnswer(t *testing.T) {
 	p1, p2, err := game.Answer()
 	assert.NoError(t, err)
 	assert.Equal(t, 2003, p1, "Part 1")
-	assert.Equal(t, -1, p2, "Part 2")
+	assert.Equal(t, 1984, p2, "Part 2")
 }
 
 func TestProcess(t *testing.T) {
@@ -32,19 +32,19 @@ func TestProcess(t *testing.T) {
 	assert.Equal(t, 5, result)
 }
 
-// func TestProcessFixed(t *testing.T) {
-// 	program := [][]byte{
-// 		[]byte("nop +0"),
-// 		[]byte("acc +1"),
-// 		[]byte("jmp +4"),
-// 		[]byte("acc +3"),
-// 		[]byte("jmp -3"),
-// 		[]byte("acc -99"),
-// 		[]byte("acc +1"),
-// 		[]byte("jmp -4"),
-// 		[]byte("acc +6"),
-// 	}
-// 	result, err := game.ProcessFixed(program)
-// 	assert.NoError(t, err)
-// 	assert.Equal(t, 5, result)
-// }
+func TestProcessFix(t *testing.T) {
+	program := [][]byte{
+		[]byte("nop +0"),
+		[]byte("acc +1"),
+		[]byte("jmp +4"),
+		[]byte("acc +3"),
+		[]byte("jmp -3"),
+		[]byte("acc -99"),
+		[]byte("acc +1"),
+		[]byte("jmp -4"),
+		[]byte("acc +6"),
+	}
+	result, err := game.ProcessFix(program)
+	assert.NoError(t, err)
+	assert.Equal(t, 8, result)
+}
